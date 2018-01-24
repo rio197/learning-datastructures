32a1315c6528ea1a9191ac47b052d00ea66ac1fe
rio197 <rio197@Kedaung-PC>
2018-01-23 23:18:19 -0500

    Finalized the main for singlylinkedlist

--- src/ds/singlylinkedlist/App.java
+++ src/ds/singlylinkedlist/App.java
@@ -10,5 +10,13 @@ package ds.singlylinkedlist;
  * @author rio197
  */
 public class App {
-    
+    public static void main(String[] args) {
+        SinglyLinkedList mylist = new SinglyLinkedList();
+        mylist.insertFirst(100);
+        mylist.insertFirst(50);
+        mylist.insertFirst(99);
+        mylist.insertFirst(88);
+        
+        mylist.displayList();
+    }
 }
