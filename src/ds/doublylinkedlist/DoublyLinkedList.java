002825dc057f6b36a85a2054b9ff0ab88cfe235b
rio197 <rio197@Kedaung-PC>
2018-01-24 20:57:20 -0500

    Added the utility methods for DoublyLinkedList

--- src/ds/doublylinkedlist/DoublyLinkedList.java
+++ src/ds/doublylinkedlist/DoublyLinkedList.java
@@ -13,5 +13,39 @@ public class DoublyLinkedList {
     private Node first;
     private Node last;
     
+    public DoublyLinkedList() {
+        this.first = null;
+        this.last = null;
+    }
     
+    public boolean isEmpty() {
+        return (first == null);
+    }
+    
+    public void insertFirst(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        
+        if (isEmpty()) {
+            last = newNode;
+        } else {
+            first.previous = newNode;
+        }        
+        
+        newNode.next = first;
+        this.first = newNode;
+    }
+    
+    public void insertLast(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+            newNode.previous = last;
+            last = newNode;
+        }
+    }
 }
